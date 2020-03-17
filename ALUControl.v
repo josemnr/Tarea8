9759243b61cbafb1785dd7e56a211e4c3342f9db
Jose Manuel <maninari97@gmail.com>
2020-03-17 03:29:33 -0600

    Adding and sorting ALU Control Values

--- ALUControl.v
+++ ALUControl.v
@@ -20,10 +20,15 @@ module ALUControl
 
 );
 
+localparam I_Type_LUI	 = 9'b011_xxxxxx;
+localparam I_Type_ANDI	 = 9'b110_xxxxxx;
+localparam R_Type_SLL	 = 9'b111_000000;
+localparam R_Type_SRL	 = 9'b111_000010;
+localparam R_Type_ADD    = 9'b111_100000;
+localparam R_Type_SUB	 = 9'b111_100010;
 localparam R_Type_AND    = 9'b111_100100;
 localparam R_Type_OR     = 9'b111_100101;
 localparam R_Type_NOR    = 9'b111_100111;
-localparam R_Type_ADD    = 9'b111_100000;
 localparam I_Type_ADDI   = 9'b100_xxxxxx;
 localparam I_Type_ORI    = 9'b101_xxxxxx;
 
@@ -34,12 +39,18 @@ assign Selector = {ALUOp, ALUFunction};
 
 always@(Selector)begin
 	casex(Selector)
+	
 		R_Type_AND:    ALUControlValues = 4'b0000;
+		I_Type_ANDI:	ALUControlValues = 4'b0000;
 		R_Type_OR: 		ALUControlValues = 4'b0001;
+		I_Type_ORI:		ALUControlValues = 4'b0001;
 		R_Type_NOR:		ALUControlValues = 4'b0010;
 		R_Type_ADD:		ALUControlValues = 4'b0011;
 		I_Type_ADDI:	ALUControlValues = 4'b0011;
-		I_Type_ORI:		ALUControlValues = 4'b0001;
+		R_Type_SUB:		ALUControlValues = 4'b0100;
+		R_Type_SLL:		ALUControlValues = 4'b0101;
+		R_Type_SRL:		ALUControlValues = 4'b0110;
+		I_Type_LUI:		ALUControlValues = 4'b0111;
 		
 		default: ALUControlValues = 4'b1001;
 	endcase
